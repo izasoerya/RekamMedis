318a4db66f7787ce2678986b376df10e3b1176b0
Soerya <ihza.surya0302@mail.ugm.ac.id>
2023-05-28 09:43:57 +0700

    first commit

---
+++ part/footer.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; 2020 <div class="bullet"></div> Aplikasi Rekam Medis | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+      
+      
+      
+    </div>
+    <div class="footer-right">
+
+    </div>
+  </footer>
