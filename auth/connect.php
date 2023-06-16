0747d126ce972ef02a946727e45d1dd1d348266c
izasoerya <122797748+izasoerya@users.noreply.github.com>
2023-06-16 22:50:31 +0700

    Update connect.php

--- auth/connect.php
+++ auth/connect.php
@@ -1,5 +1,5 @@
 <?php
 	#urutane "localhost", "username mu", "password mu", "jeneng db mu"
 	
-	$conn = mysqli_connect("localhost", "root", "", "rekam_medis");
+	$conn = mysqli_connect("localhost", "root", "9182736455", "rekam_medis");
 ?>
