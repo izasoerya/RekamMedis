0171060848cbf06eebbd66848c2b9f86eea86e15
izasoerya <122797748+izasoerya@users.noreply.github.com>
2023-06-17 11:13:25 +0700

    Update connect.php

--- auth/connect.php
+++ auth/connect.php
@@ -1,5 +1,5 @@
 <?php
 	#urutane "localhost", "username mu", "password mu", "jeneng db mu"
 	
-	$conn = mysqli_connect("localhost", "root", "social empires3", "rekam_medis");
+	$conn = mysqli_connect("localhost", "root", "admin", "rekam_medis");
 ?>
